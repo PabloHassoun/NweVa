5440a166551f1b8e385dbc335cb202be0d53592b
Pablo <baienwarr@icloud.com>
2025-06-16 23:48:35 -0400

    PreBuilding0.0

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div
+      id="home"
+      className="flex flex-row items-center justify-center gap-5 h-screen"
+    >
+      <Image
+        src="/assets/svg/logo.svg"
+        alt="Logo"
+        width={35.54}
+        height={77.5}
+      />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-7xl font-title text-white">NweVa</h1>
+        <p className="text-2xl font-text text-white">Web & AI Innovation</p>
+      </div>
+      <Image
+        src="/assets/svg/logo.svg"
+        alt="Logo"
+        width={35.54}
+        height={77.5}
+        className="rotate-180"
+      />
+    </div>
+  );
 }
